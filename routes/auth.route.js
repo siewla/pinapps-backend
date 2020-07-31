7ce650f6caa24b11503df0caabc2b82db871ac67
siewla <siewlapang@gmail.com>
2020-08-01 02:50:56 +0800

    google login completed

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,11 +2,20 @@ const express   =   require('express');
 const router    =   express.Router();
 
 //Load Controllers
-const { registerController, activationController, loginController } = require('../controllers/auth.controller.js');
+const { 
+    registerController, 
+    activationController, 
+    loginController, 
+    forgetController,
+    resetController,
+    googleController } = require('../controllers/auth.controller.js');
 
 router.post('/register', registerController);
 router.post('/activation', activationController);
 router.post('/login', loginController);
+router.put('/password/forget', forgetController);
+router.put('/password/reset', resetController);
+router.post('/googlelogin', googleController);
 
 
 module.exports = router;
