fcd7a325a1b5cec237f10e8dee8dda63073e0678
neocheekiong <neocheekiong@gmail.com>
2020-07-31 20:08:53 +0800

    played around with capture-website

---
+++ screenshots/index.js
@@ -0,0 +1,24 @@
+const screenshots = require('capture-website');
+const captureURL = 'https://www.facebook.com';
+const uploadFolder = 'screenshots/'
+
+/**
+ * Cloudinary Config
+ */
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+    cloud_name: 'apcb',
+    api_key: '331716551633671',
+    api_secret: process.env.CLOUDINARY_SECRET
+});
+const websiteCaptureOptions = {
+    width: 320,
+    height: 400
+}
+
+let index = `${Math.random()}1.jpg` //ObjectId?
+screenshots.base64(captureURL, websiteCaptureOptions).then((result) => console.log(result)).catch(err => console.log(err)) //.then(
+//     (file) => {
+//         cloudinary.uploader.upload(file).catch(err => console.log(err));
+//     }
+// ).catch(err => console.log(err))
