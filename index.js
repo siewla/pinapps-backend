2bbbcf5394b8f621c4d0f027e1d68f0c137ca52e
siewla <siewlapang@gmail.com>
2020-08-01 22:07:27 +0800

    fix error bracket

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === 'dev') {
 
 /*------------Route------------*/
 //Load All routes
-app.use('/api/auth/', require('./routes/auth.route');
+app.use('/api/auth/', require('./routes/auth.route'));
 app.use('/api/apps/', require('./routes/apps.routes'));
 app.use('/api/users/', require('./routes/users.routes'));
 // require('./routes')(app);
