24feb3497e89115cdb537bda7357496218ba2c3f
neocheekiong <neocheekiong@gmail.com>
2020-08-01 22:46:55 +0800

    added users route into router

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,5 @@ const appControllers = require('../controllers');
 module.exports = (app) => {
     app.use('/auth', require('./auth.routes'));
     app.use('/apps', require('./apps.routes'));
+    app.use('/users', require('./users.routes'));
 };
